51cff499d4ef24bd3d4d83c40e0c4037cd654192
Марина <77566874+marizhura@users.noreply.github.com>
2023-07-28 15:36:44 +0300

    lesson work 2

---
+++ Login/Login/String+Ex.swift
@@ -0,0 +1,14 @@
+//
+//  String+Ex.swift
+//  Login
+//
+//  Created by Марина Журавлева on 27.07.2023.
+//
+
+import Foundation
+
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
--- Login/Login/ViewController.swift
+++ Login/Login/ViewController.swift
@@ -10,36 +10,60 @@ import UIKit
 class ViewController: UIViewController {
     
     // MARK: - IBOutlets
-    @IBOutlet weak var mailText: UITextField!
-    @IBOutlet weak var mailImageView: UIImageView!
-    @IBOutlet weak var mailView: UIView!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var emailImageView: UIImageView!
+    @IBOutlet weak var emailView: UIView!
     @IBOutlet weak var passwordText: UITextField!
     @IBOutlet weak var passwordImageView: UIImageView!
     @IBOutlet weak var passwordView: UIView!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var signupButton: UIButton!
     
+    // MARK: - Properties
+    
+    private var email: String = ""
+    private var password: String = ""
+    private var mockEmail = "abc@mail.com"
+    private var mockPassword = "123456"
+    
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupLoginButton()
-        
-        //Настройка кнопки Login
-        loginButton.layer.cornerRadius = 10
-        loginButton.layer.shadowColor = UIColor.systemGray.cgColor
-        loginButton.layer.shadowOffset = CGSize(width: 2, height: 2)
-        loginButton.layer.shadowOpacity = 0.8
-        loginButton.layer.shadowRadius = 4
+        emailText.delegate = self
+        passwordText.delegate = self
+        emailText.becomeFirstResponder()
     }
     
     // MARK: - IBActions
     @IBAction private func loginAction(_ sender: Any) {
-        performSegue(withIdentifier: "goToHomePage", sender: sender)
+        emailText.resignFirstResponder()
+        passwordText.resignFirstResponder()
+        
+        if email.isEmpty {
+            makeErrorField(textField: emailText)
+        }
+        
+        if password.isEmpty {
+            makeErrorField(textField: passwordText)
+        }
+        
+        if email == mockEmail,
+           password == mockPassword {
+            performSegue(withIdentifier: "goToHomePage", sender: sender)
+        } else {
+            let alert = UIAlertController(title: "Error".localized,
+                                          message: "Wrong password or e-mail".localized, preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK".localized, style: .default)
+            alert.addAction(action)
+            
+            present(alert, animated: true)
+        }
     }
     
     @IBAction private func signupAction(_ sender: Any) {
-        print("Кнопка Signup here была нажата!")
+        print("Signup")
     }
     
     // MARK: - Private methods
@@ -49,6 +73,64 @@ class ViewController: UIViewController {
         loginButton.layer.shadowOffset = CGSize(width: 0, height: 8)
         loginButton.layer.shadowOpacity = 0.4
         loginButton.layer.shadowRadius = 8
+        
+        loginButton.isUserInteractionEnabled = false
+        loginButton.backgroundColor = .systemGray3
     }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+              !text.isEmpty else { return }
+        switch textField {
+        case emailText:
+            let isValidEmail = check(email: text)
+            loginButton.isUserInteractionEnabled = isValidEmail
+            loginButton.backgroundColor = isValidEmail ? UIColor(named: "priceColor") : .systemGray5
+            
+            if isValidEmail {
+                email = text
+                emailImageView.tintColor = .systemGray3
+                emailView.backgroundColor = .systemGray3
+            } else {
+                makeErrorField(textField: textField)
+            }
+        case passwordText:
+            let isValidPassword = check(password: text)
+            loginButton.isUserInteractionEnabled = isValidPassword
+            loginButton.backgroundColor = isValidPassword ? UIColor(named: "priceColor") : .systemGray5
+            
+            if isValidPassword {
+                password = text
+                passwordImageView.tintColor = .systemGray3
+                passwordView.backgroundColor = .systemGray3
+            } else {
+                makeErrorField(textField: textField)
+            }
+        default:
+            print("Unknown textField")
+        }
+    }
+    
+    private func check(email: String) -> Bool {
+        return email.contains("@") && email.contains(".")
+    }
+    
+    private func check(password: String) -> Bool {
+        return password.count >= 4
+    }
+    
+    private func makeErrorField(textField: UITextField) {
+        switch textField {
+        case emailText:
+            emailImageView.tintColor = .systemRed
+            emailView.backgroundColor = .systemRed
+        case passwordText:
+            passwordImageView.tintColor = .systemRed
+            passwordView.backgroundColor = .systemRed
+        default:
+            print("Unknown textField")
+        }
+    }
+}
